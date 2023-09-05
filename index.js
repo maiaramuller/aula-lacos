91553c718970e38d5fe1ec95631acfee390d47e2
maiara <maiara.15muller@outlook.com>
2023-09-04 21:48:54 -0300

    inicios dos exercícios

---
+++ index.js
@@ -0,0 +1,30 @@
+//Exercícios de interpretação de código
+
+//1.
+/*
+eles esta somando todos os numeros menores que 5. Será impresso no console 10.
+
+*/
+
+//2.
+//A) todos os numeros maiores que 18.
+//B)Não, o certo é utilizar for.
+
+//3.
+/*
+ *
+ **
+ ***
+ ****
+ */
+
+//Exercícios de escrita de código
+
+//1.
+
+const bichoEstimacao = Number(prompt("Quantos animais de estimação vc tem?"));
+
+for (let i = 0; i > bichoEstimacao; i++) {
+  const teste1 = prompt("Dige o nome do seu animal");
+}
+console.log(teste1);
