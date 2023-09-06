f6630722385a6cd45cd1d84e5735c9680816a161
maiara <maiara.15muller@outlook.com>
2023-09-05 21:52:17 -0300

    exercícios em andamento

--- index.js
+++ index.js
@@ -21,10 +21,49 @@ eles esta somando todos os numeros menores que 5. Será impresso no console 10.
 //Exercícios de escrita de código
 
 //1.
-
-const bichoEstimacao = Number(prompt("Quantos animais de estimação vc tem?"));
-
-for (let i = 0; i > bichoEstimacao; i++) {
-  const teste1 = prompt("Dige o nome do seu animal");
+/*
+let bichoEstimacao = Number(prompt("Quantos animais de estimação vc tem?"));
+let arrays = [];
+if (0 === bichoEstimacao) {
+  console.log("Que pena! Você pode adotar um pet!");
+} else {
+  for (let i = 0; i < bichoEstimacao; i++) {
+    const array = prompt("Digite o nome dos seus animais:");
+    arrays.push(array);
+  }
+  console.log(arrays);
 }
-console.log(teste1);
+*/
+
+//2.
+const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
+/*
+const arrayOriginal = () => {
+  for (const arr of array) {
+    console.log(arr);
+  }
+};
+arrayOriginal();
+*/
+/*
+const arrayDivididoDez = () => {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    const divisao = element / 10;
+    console.log(divisao);
+  }
+};
+arrayDivididoDez();
+*/
+
+const arrayPar = () => {
+  let novoArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (element % 2 === 0) {
+      novoArray.push(array);
+      console.log(array[i]);
+    }
+  }
+};
+arrayPar();
